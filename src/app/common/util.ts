ba12573fc863fec267d4d56e935028c1978465cd
huynhnhu <huynhnhu@VN-HCM-D-M-0019.local>
2021-02-25 17:57:12 +0700

    feat: Essential RxJs Operators + Reactive Design

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,14 @@
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
-
+export const fetchCoursesHttp = (url) => new Observable(observer => {
+  fetch(url)
+    .then(res => res.json())
+    .then(body => {
+      observer.next(body);
+      observer.complete();
+    })
+    .catch(error => {
+      observer.error();
+    })
+});
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,7 +16,7 @@ import {
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 
-
+import { fetchCoursesHttp } from '../common/util';
 @Component({
     selector: 'course',
     templateUrl: './course.component.html',
@@ -27,6 +27,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     course$: Observable<Course>;
     lessons$: Observable<Lesson[]>;
+    courseId: any;
 
 
     @ViewChild('searchInput', { static: true }) input: ElementRef;
@@ -38,17 +39,31 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
 
-        const courseId = this.route.snapshot.params['id'];
+        this.courseId = this.route.snapshot.params['id'];
 
-
-
+        this.course$ = fetchCoursesHttp(`/api/courses/${this.courseId}`)
+                            .pipe(map((res: Course) => res))
     }
 
     ngAfterViewInit() {
 
+        const searchLessons$ = fromEvent(this.input.nativeElement, 'input')
+                                .pipe(
+                                    map((event: any) => event.target.value),
+                                    debounceTime(400),
+                                    distinctUntilChanged(),
+                                    switchMap(val => this.searchLessons(val))
+                                );
+        const initialLessons$ = this.searchLessons();
 
+        this.lessons$ = concat(initialLessons$, searchLessons$);
+    }
 
-
+    searchLessons(search = ''): Observable<Lesson[]> {
+        return fetchCoursesHttp(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
+        .pipe(
+            map(res => res['payload'])
+        )
     }
 
 
